635c0b2d0f634edd942adc176d2fcaa07b90d999
y2Dev <dub-y@tuta.io>
2022-09-13 12:14:20 -0400

    navigation de la connexion à travers la pression simple d'un bouton

--- allApps/Ecommerce/public/Connexion.js
+++ allApps/Ecommerce/public/Connexion.js
@@ -1,10 +1,23 @@
 import { Text, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Button } from "@rneui/themed";
 
 const Connexion = () => {
+
+  const navigation = useNavigation() ;
+
+  const goToInscription = () => {
+    navigation.navigate('Inscription') ;
+  }
+
   return (
     <View>
       <Text>Connexion</Text>
+      <Button
+        onPress={goToInscription}
+        title="Inscrivez-vous !"
+        type="clear" />
     </View>
   )
 }
