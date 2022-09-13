fba963285d276ff9456b160f40b03038b8e19ab0
y2Dev <dub-y@tuta.io>
2022-09-13 12:36:50 -0400

    ajout d'entrée pour l'email d'identification

--- allApps/Ecommerce/public/Inscription.js
+++ allApps/Ecommerce/public/Inscription.js
@@ -1,10 +1,36 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Button, Input, Icon } from "@rneui/themed";
 
 const Inscription = () => {
+
+    const navigation = useNavigation() ;
+
+    const goToConnexion = () => {
+        navigation.navigate('Connexion') ;
+      }
+
   return (
     <View>
       <Text>Inscription</Text>
+
+      <Input
+      placeholder='Votre e-mail'
+      leftIcon={
+        <Icon
+          name='email'
+          type='entypo'
+          size={24}
+          color='black'
+        />
+      }
+    />
+
+      <Button
+        onPress={goToConnexion}
+        title="Connexion"
+        type="clear" />
     </View>
   )
 }
