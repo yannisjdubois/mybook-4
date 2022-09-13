419f2988ed95f3cbfca64db49468d1d207a6f628
y2Dev <dub-y@tuta.io>
2022-09-13 14:21:10 -0400

    ajout d'une icône de masquage du mot de passe

--- allApps/Ecommerce/public/Inscription.js
+++ allApps/Ecommerce/public/Inscription.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Button, Input, Icon } from "@rneui/themed";
 
 const Inscription = () => {
 
     const navigation = useNavigation() ;
+
+    const [AffMotDePasse, setAffMotDePasse] = useState(true)
 
     const goToConnexion = () => {
         navigation.navigate('Connexion') ;
@@ -18,18 +20,39 @@ const Inscription = () => {
       <Input
       placeholder='Votre e-mail'
       leftIcon={
-        <Icon
-          name='email'
-          type='entypo'
-          size={24}
-          color='black'
+            <Icon
+            name='alternate-email'
+            type='materialicons'
+            size={24}
+            color='black'
+            />
+        }
         />
-      }
-    />
+
+      <Input
+      placeholder='Votre mot de passe'
+      secureTextEntry={true}
+      leftIcon={
+            <Icon
+            name='lock'
+            type='materialicons'
+            size={24}
+            color='black'
+            />
+        }
+      rightIcon={
+            <Icon
+            name='eye-with-line'
+            type='entypo'
+            size={24}
+            color='black'
+            />
+        }
+        />
 
       <Button
         onPress={goToConnexion}
-        title="Connexion"
+        title="Je me connecte !"
         type="clear" />
     </View>
   )
